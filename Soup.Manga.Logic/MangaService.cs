c39536d69e4480c5440cfdfa84e0a7e3007fda83
Can Ofsoup <Milancurinckx@gmail.com>
2025-02-22 12:13:32 +0100

    Add project files.

---
+++ Soup.Manga.Logic/MangaService.cs
@@ -0,0 +1,66 @@
+﻿
+using Soup.Manga.Objects;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Soup.Manga.Logic
+{
+    public class MangaService
+    {
+        private string _path = @"C:\users\Milan\Desktop\MangaTest\mangaTest";
+        public List<Soup.Manga.Objects.Manga> GetMangas()
+        {
+            List<Soup.Manga.Objects.Manga> mangaList;
+            if (File.Exists(_path))
+            {
+            var json = File.ReadAllText(_path);    
+            mangaList = System.Text.Json.JsonSerializer.Deserialize<List<Objects.Manga>>(json);
+            }
+            else
+            {
+                mangaList = new()
+                {
+                    new Objects.Manga
+                    {
+                      Id = 1, Name = "TestVolume"
+                    }
+                };
+               SaveManga(mangaList);
+            }
+            return mangaList;
+        }
+
+        public void removeManga(Objects.Manga manga)
+        {
+            List<Objects.Manga> mangaList = GetMangas();
+            mangaList.Remove(manga);
+            SaveManga(mangaList);
+        }
+
+        private void SaveManga(List<Objects.Manga> mangaList)
+        {
+            var json = JsonSerializer.Serialize(mangaList);
+            File.WriteAllText(_path, json);
+        }
+        public void addManga(Objects.Manga manga)
+        {
+            List<Objects.Manga> mangaList = GetMangas();
+            mangaList.Add(manga);
+            SaveManga(mangaList);
+        }
+
+        public void UpdateManga(Objects.Manga manga)
+        {
+            List<Objects.Manga> mangaList = GetMangas();
+            Objects.Manga toUpdateManga = mangaList.FirstOrDefault(m => m.Id == manga.Id);
+            mangaList.Remove(toUpdateManga);
+            mangaList.Add(manga);
+            SaveManga(mangaList);
+        }
+
+        public Objects.Manga GetMangaById(int id)
+        {
+            return GetMangas().FirstOrDefault(m => m.Id == id);   
+        }
+    }
+}
---
+++ Soup.Manga.UI/Controllers/MangaController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Soup.Manga.Logic;
+
+namespace Soup.Manga.UI.Controllers
+{
+    public class MangaController : Controller
+    {
+        private MangaService _MangaService = new MangaService();
+        public IActionResult Index()
+        {
+            return View(_MangaService.GetMangas());
+        }
+        [HttpPost]
+        public IActionResult Edit(Objects.Manga manga)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Error");
+            }
+            _MangaService.UpdateManga(manga);
+            return View(manga);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return View(_MangaService.GetMangaById(id));
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(_MangaService.GetMangaById(id));
+        }
+
+        //The logic writes to a json and this just grabs that json to show
+        public IActionResult GetData()
+        {
+            var json = JsonConvert.SerializeObject(_MangaService.GetMangas());
+            return Json(json);
+        }
+    }
+}
