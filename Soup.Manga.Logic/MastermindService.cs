602935eadcab8b95fca0adba20219effc460e22d
Can Ofsoup <Milancurinckx@gmail.com>
2025-02-23 19:33:24 +0100

    Fixed the MasterMind logic and added comment

--- Soup.Manga.Logic/MastermindService.cs
+++ Soup.Manga.Logic/MastermindService.cs
@@ -6,34 +6,37 @@ using System.Threading.Tasks;
 
 namespace Soup.Manga.Logic
 {
+    
     public class MastermindService
     {
         Random Random = new Random();
         public int TargetNumber { get; set; }
-        private bool _IsNumberGenerated = false;
+        private bool _IsNumberGenerated = true;
 
+        
         public string GuessNumber(int aNumber)
         {
+            string result;
+            //Put this in a bool so it only does it once per guess
             if (_IsNumberGenerated)
             {
                 TargetNumber = Random.Next(10);
-                _IsNumberGenerated=true;
+                _IsNumberGenerated=false;
             }
-            
-            string result;
-            if (aNumber == TargetNumber)
-            {
-                result = "Wow, I can't believed you actually guessed it";
-            }
+
             if (aNumber > TargetNumber)
             {
                 result = "Hmmm, No. Try picking something a little smaller";
             }
-            else
+            else if (aNumber < TargetNumber)
             {
                 result = "Hmmm, No. Try picking something a little bigger";
             }
-            return result;
+            else
+            {
+                result = "Wow, I can't believed you actually guessed it";
+            }
+           return result;
             
         }
     }
